c7df335f17f9d4ee96afdfa41bb5f02d056de051
dsudaez <db.ing@hotmail.com>
2016-05-05 23:46:59 -0300

    Update arbolesdeb.R
    Agrego la parte de los gráficos y corrijo error en el promedio de errores

--- tp5/arbolesdeb.R
+++ tp5/arbolesdeb.R
@@ -1,7 +1,5 @@
 #1
 set.seed(134678)
-library(randomForest)
-
 X1 <- cbind(c(rnorm(50,8,2)), c(rnorm(50,8,2)), c(rep(1,50)));
 X2 <- cbind(c(rnorm(50,11,2)), c(rnorm(50,11,2)), c(rep(0,50)));
 matriz <- rbind(X1,X2)
@@ -17,8 +15,8 @@ plot(0:14,0.:14,type='n',main='Dataset Test',xlab = 'X1',ylab = 'X2')
 points(dataset_test[1:15,1],dataset_test[1:15,2], col='red')
 points(dataset_test[16:30,1],dataset_test[16:30,2], col='blue')
 dev.off()
-#4
-#dataset_train[,3]
+
+#4 
 numTree=1
 #errors <- list()
 errorMean <- array()
@@ -27,12 +25,14 @@ for(i in 1:7){
   set.seed(134678)
   models[[i]] <- randomForest(x=dataset_train[,-3], y=as.factor(dataset_train[,3]), xtest=dataset_test[,-3],ytest=as.factor(dataset_test[,3]),ntree=numTree)
   #errors[[i]] <- models[[i]]$err.rate[,1]
-  errorMean[i] <- mean(models[[i]]$err.rate[,1])
+  errorMean[i] <- mean(models[[i]]$test$err.rate[,1])
   numTree <- numTree*10
 }
-#4
-#Se puede observar que el promedio de errores de cada ensamble es:
-#[1] 0.1250000 0.1647143 0.1383286 0.1363614 0.1404561 0.1419866 0.1431814
-#
+#5
+#La conclusión está en el PDF que está adjunto.
 #6
-
+pdf("Grafica_predicciones_RF.pdf")
+plot(0:20,0.:20,type='n',main='Predicciones RF',xlab = 'X1',ylab = 'X2')
+points(dataset_test[models[[3]]$test$predicted==1,1],dataset_test[models[[3]]$test$predicted==1,2], col='red')
+points(dataset_test[models[[3]]$test$predicted==0,1],dataset_test[models[[3]]$test$predicted==0,2], col='blue')
+dev.off()
